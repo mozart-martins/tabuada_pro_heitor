a7192b881d322e4d56ab517fe35933b59a832e62
mozart-martins <49461099+mozart-martins@users.noreply.github.com>
2022-06-03 11:13:58 -0300

    Tabuada até 10

--- jogo_do_heitor.py
+++ jogo_do_heitor.py
@@ -4,9 +4,9 @@ quantidade = 0
 quantidade = int(input("Quantas continhas você quer fazer? "))
 
 for q in range(quantidade):
-    print(str(random.randint(1,5)))
-    numero1 = random.randint(1,5)
-    numero2 = random.randint(1,5)
+    print(str(random.randint(1,10)))
+    numero1 = random.randint(1,10)
+    numero2 = random.randint(1,10)
     resposta = int(input(f"{numero1} x {numero2} = "))
     if resposta == (numero1 * numero2):
         print(f"Você ACERTOU! A respostas é {resposta}!!")
